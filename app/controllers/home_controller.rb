b805739b03bf372dde3806b4a2a78ab3271254a0
Abhishek Patel <abhishek.patel@harbingergroup.com>
2012-04-04 12:34:12 +0530

    inital commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+class HomeController < ApplicationController
+
+  def publish
+    
+  end
+
+  def get
+    
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RabbitRails::Application.config.secret_token = '0d4feda1befdcc8aa95ea0273c6fc8eaa7ecce7fa1f80d5cfb70f9117745aa56f6240086eb693c051b75c51b8c5d6b68becc3bc74e5e569a08ef5e4fad7e7ea5'
